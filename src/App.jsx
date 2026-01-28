9a062412f278dca2c323997fe66ab89c0b5b9941
devyos <L21170485@culiacan.tecnm.mx>
2026-01-27 17:32:12 -0700

    add import redirect page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import RedirectPage from "./pages/RedirectPage";
 import ErrorPage from "./pages/Error";
 
 export default function App() {
